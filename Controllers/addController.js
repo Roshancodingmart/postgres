f4e99a6fa0e391dc393121f3210784ffbafaff9b
Roshan <59079952+Roshancodingmart@users.noreply.github.com>
2020-01-22 10:53:49 +0530

    Add files via upload

---
+++ Controllers/addController.js
@@ -0,0 +1,24 @@
+const conn = require("../db")
+
+conn.client.connect(function (err){
+    if(err)
+        console.log(err,"not connected");
+    else
+        console.log("Connected!");
+  });
+const createUser=(req,res)=>{
+    const{b}=req.body;
+    console.log(b.id,b.name,b.pass)
+    conn.client.query(`INSERT INTO public.user(id,username,password) VALUES ($1 , $2 , $3)`,[b.id,b.name,b.pass],(error,res)=>{
+        if(error){
+            console.log(error)
+        }
+       else{
+           console.log(res)
+          console.log("User added to database")
+       }
+    })
+}
+module.exports={
+    createUser
+}
